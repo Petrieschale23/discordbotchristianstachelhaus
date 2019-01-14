30d9065de04006d259b1c7a7d8dcfc5442c4e5af
Petrieschale23 <46688141+Petrieschale23@users.noreply.github.com>
2019-01-14 21:17:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+    }
     if (message.content === 'Hallo') {
     	message.reply('Fick Dich');
     
