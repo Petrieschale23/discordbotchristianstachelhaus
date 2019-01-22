5296e0bc8e6b02c6d4daabd90f659caf876d20f4
Petrieschale23 <46688141+Petrieschale23@users.noreply.github.com>
2019-01-22 20:46:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,32 +12,7 @@ client.on('message', message => {
     if (message.content === 'Hallo') {
     	message.reply('Willkommen auf dem Chrisultra Discord du geile Schnitte, ich (Gott) wünsche dir viel Spaß auf dem Discord, bitte beachte die Regeln oder ich muss dich leider vernichten.');
     
-    }
-    if (message.content === 'Regeln') {
-    	message.reply('1. Ihr verliert kein Wort über die #ChrisUltras'     
-                      '2. Ladet jeden ein'     
-                      '3. Ruft jmd "HALT, STOP!" heisst er Andreas'     
-                      '4. Kommentiert wird überall'     
-                      '5. Nur ein Kommentar/Video'     
-                      '6. Keine Hemden, keine Schuhe'     
-                      '7. Kommentare sind so lang, wie sie sein müssen'     
-                      '8. Wer neu ist, muss kommentieren');
-    }
-    if (message.content === 'Peter') {
-    	message.reply('Fels');
-    }
-     if (message.content === 'Jay') {
-    	message.reply('Salz');
-    }
-     if (message.content === 'Brammen') {
-    	message.reply('=B43');
-    }
-     if (message.content === 'Chris') {
-    	message.reply('=Hallöchen');
-    }
-     if (message.content === 'Gott') {
-    	message.reply('=Hallöchen');
-    }
+   
 });
 
 // THIS  MUST  BE  THIS  WAY
