9eb6260817ef03797ec80356fa119fc8c8c2f4bf
Petrieschale23 <46688141+Petrieschale23@users.noreply.github.com>
2019-01-22 18:17:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,14 @@ client.on('message', message => {
     
     }
     if (message.content === 'Regeln') {
-    	message.reply('1. Ihr verliert kein Wort über die #ChrisUltras     2. Ladet jeden ein     3. Ruft jmd "HALT, STOP!" heisst er Andreas     4. Kommentiert wird überall     5. Nur ein Kommentar/Video     6. Keine Hemden, keine Schuhe     7. Kommentare sind so lang, wie sie sein müssen     8. Wer neu ist, muss kommentieren');
+    	message.reply('1. Ihr verliert kein Wort über die #ChrisUltras     
+                      '2. Ladet jeden ein     
+                      '3. Ruft jmd "HALT, STOP!" heisst er Andreas     
+                      '4. Kommentiert wird überall     
+                      '5. Nur ein Kommentar/Video     
+                      '6. Keine Hemden, keine Schuhe     
+                      '7. Kommentare sind so lang, wie sie sein müssen     
+                      '8. Wer neu ist, muss kommentieren');
     }
     if (message.content === 'Peter') {
     	message.reply('=Fels');
